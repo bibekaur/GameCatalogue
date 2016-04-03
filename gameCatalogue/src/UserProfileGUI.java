eece097c771e22c1a5da96c43c62d29b0d47d968
bibekaur <bibekaur@gmail.com>
2016-04-02 17:29:09 -0700

    Adding (semi-implemented) developer info page

--- gameCatalogue/src/UserProfileGUI.java
+++ gameCatalogue/src/UserProfileGUI.java
@@ -1,12 +1,9 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 
 
